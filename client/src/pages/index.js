cc9b476149b2197660dbc17d3701cf7633301c63
ashleyth0mas <ashmariam@hotmail.com>
2023-05-31 10:48:34 +0530

    revert commit

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,15 +1,10 @@
 //import { ConnectWallet } from "@thirdweb-dev/react";
 
 //import styles from "../styles/Home.module.css";
-import Marketplace from "../components/Marketplace";
+import Dashboard from "../components/Dashboard";
 import Header from "../components/Header";
-import Navbar from "../components/Navbar";
 import { Homepg } from "../components/Homepg";
-import ExpertDashboard from "../components/ExpertDashboard";
-import Expertnfts from "../components/Expertnfts";
-import Expert_uploadcourses from "../components/Expert_uploadcourses";
-import Student_dashboard from "../components/Student_dashboard";
-import Studentcourses from "../components/Student_courses";
+
 
 
 
@@ -19,15 +14,9 @@ export default function Home() {
  
    {/* <Header/>
    
-    <Homepg/> 
-       <Marketplace/> 
-       <ExpertDashboard/>
-       <Navbar/>
-       <Expertnfts/>
-       <Student_dashboard/>
-       <Expert_uploadcourses/>
-        */}
-      <Studentcourses/>
+    <Homepg/>*/} 
+       <Dashboard/>
+       
    
     </div>
   );
